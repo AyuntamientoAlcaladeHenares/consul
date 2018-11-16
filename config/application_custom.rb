2da3b05b025ff99bd959293c0b51a3fe418ec365
Eduardo Martínez <eduardomech@gmail.com>
2018-11-16 16:31:09 +0100

    Set default locale and available locales of application

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,5 +2,13 @@ module Consul
   class Application < Rails::Application
     require Rails.root.join('lib/custom/sms_api')
     require Rails.root.join('lib/custom/census_api')
+
+    config.i18n.default_locale = :es
+
+    if Rails.env.production?
+      config.i18n.available_locales = [:es]
+    else
+      config.i18n.available_locales = [:es, :en]
+    end
   end
 end
